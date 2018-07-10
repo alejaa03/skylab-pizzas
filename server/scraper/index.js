cf4631c7b07ab222d705ef90fff8ce2f9a2dde46
Alejandro Pérez <aleperjim98@gmail.com>
2018-07-10 13:32:01 +0200

    Fix scrapper

--- server/scraper/index.js
+++ server/scraper/index.js
@@ -23,7 +23,7 @@ scraperRouter.route('/')
             var options = {
                 url,
                 headers: {
-                    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36'
+                      'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36'
                 }
             };
             request(options, function (error, response, html) {
